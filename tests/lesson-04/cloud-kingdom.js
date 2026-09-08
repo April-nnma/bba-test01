1de67a14d0f9f8925c591a9a2a3c3947c008a03a
test 01 - Anh Nguyen <nguyenanh1904.vn@gmail.com>
2026-09-08 13:21:44 +0700

    feat: add solution for challenge 03

---
+++ tests/lesson-04/cloud-kingdom.js
@@ -0,0 +1,11 @@
+let playerName = "Mario";
+let currentLives = 3;
+const Level1 = 25;
+const Level2 = 30;
+const Level3 = 45;
+
+const total = Level1 + Level2 + Level3;
+const avg = total / 3;
+const remainder = total % 3;
+console.log(remainder);
+
